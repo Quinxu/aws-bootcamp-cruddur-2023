be31d1656549b19d6ed9c0086e89b5500f767e4d
Qin Xu <quinxu@hotmail.com>
2023-02-27 20:19:30 +0000

    Added lines for sending data to backend-flask logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -21,12 +21,19 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+
 
 # HoneyComb ----------------
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+# Show this in the logs with the backend flask app (STDOUT)
+simpleProcessor = SimpleSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(simpleProcessor)
+
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
