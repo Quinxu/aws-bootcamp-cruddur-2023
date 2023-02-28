d7b273d136d1352b40db3517b3d2474dfe1b1816
Qin Xu <quinxu@hotmail.com>
2023-02-28 19:06:26 +0000

    acquired tracer and added spans attributes

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,8 +1,21 @@
 from datetime import datetime, timedelta, timezone
+
+#Acquiring a Tracer
+from opentelemetry import trace
+tracer = trace.get_tracer("home.activities")
+#
 class HomeActivities:
   def run():
-    now = datetime.now(timezone.utc).astimezone()
-    results = [{
+    #Creating Spans 
+    with tracer.start_as_current_span("home-activities-mock-data"):
+    #
+      #Adding Attributes to Spans
+      span = trace.get_current_span()
+      now = datetime.now(timezone.utc).astimezone()
+      span.set_attribute("app.now", now.isoformat())
+      #
+      
+      results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
       'message': 'Cloud is fun!',
@@ -41,4 +54,5 @@ class HomeActivities:
       'replies': []
     }
     ]
+      span.set_attribute("app.result_length", len(results))
     return results
