d3d87cbb500ca98d78c1c22fc240e46804b4e8d3
Qin Xu <quinxu@hotmail.com>
2023-04-11 05:35:18 +0000

    added users_short code, message group new page,

---
+++ backend-flask/services/users_short.py
@@ -0,0 +1,8 @@
+from lib.db import db
+
+class UsersShort:
+  def run(handle):
+    sql = db.template('db/sql/users','short.sql')
+    results = db.query_json_object(sql,
+      handle= handle)
+    return results
