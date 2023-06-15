3b737ee7399807c9a0a8b6797c685212854a18fc
Qin Xu <quinxu@hotmail.com>
2023-06-15 19:20:17 +0000

    updated cdk stacks.ts, .env and .gitignore only for frontend and backend .env files.

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,7 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import * as dotenv from 'dotenv';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+
+dotenv.config();
 
 export class ThumbingServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,10 +18,13 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'ThumbingServerlessCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+    const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
+    const folderInput: string = process.env.THUMBING_S3_FOLDER_INPUT as string;
+    const folderOutput: string = process.env.THUMBING_S3_FOLDER_OUTPUT as string;
 
-    const buckName: string = process.env.THUMBING_BUCKET_NAME as string;
-    const bucket= this.createBucket(buckName);
-
+    const bucket= this.createBucket(bucketName);
+    const lambda= this.createLambda(functionPath, bucketName, folderInput, folderOutput);
 
   }
 
@@ -28,6 +36,19 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     return bucket;
   }
 
+  createLambda(functionPath:string, bucketName: string, folderInput: string, folderOutput: string):lambda.IFunction{
+    const lambdaFunction = new lambda.Function(this, 'ThumbLambda',{
+      runtime:lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code:lambda.Code.fromAsset(functionPath),
+      environment: {
+        DEST_BUCKET_NAME: bucketName,
+        FOLDER_INPUT: folderInput,
+        FOLDER_OUTPUT:  folderOutput,
+        PROCESS_WIDTH: '512',
+        PROCESS_HEIGHT: '512'
+      }
+    });
+    return lambdaFunction;
+  }
 }
-
-
