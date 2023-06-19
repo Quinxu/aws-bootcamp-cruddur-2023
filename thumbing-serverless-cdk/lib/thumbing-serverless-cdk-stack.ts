37f3d555fe8765e3dd61dd637b326deee87cffe9
Qin Xu <quinxu@hotmail.com>
2023-06-19 20:14:42 +0000

    added lambda functions, build script, S3notificationToLambda in .ts.

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -4,7 +4,11 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import * as dotenv from 'dotenv';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as process from 'process';
 
 dotenv.config();
 
@@ -22,19 +26,39 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
     const folderInput: string = process.env.THUMBING_S3_FOLDER_INPUT as string;
     const folderOutput: string = process.env.THUMBING_S3_FOLDER_OUTPUT as string;
+    const webhookUrl: string = process.env.THUMBING_WEBHOOK_URL as string;
+    const topicName: string = process.env.THUMBING_TOPIC_NAME as string;
 
     const bucket= this.createBucket(bucketName);
     const lambda= this.createLambda(functionPath, bucketName, folderInput, folderOutput);
 
+     // This could be redundent since we have s3ReadWritePolicy?
+   //  bucket.grantRead(lambda);
+   //  bucket.grantPut(lambda);
+ /*
+     const snsTopic = this.createSnsTopic(topicName)
+     this.createSnsSubscription(snsTopic,webhookUrl)
+ 
+     // S3 Event Notifications
+     this.createS3NotifyToSns(folderOutput,snsTopic,bucket)  */
+     this.createS3NotifyToLambda(folderInput,lambda,bucket)
+ /*
+     const s3ReadWritePolicy = this.createPolicyBucketAccess(bucket.bucketArn)
+     const snsPublishPolicy = this.createPolicySnSPublish(snsTopic.topicArn)
+ 
+     lambda.addToRolePolicy(s3ReadWritePolicy);
+     lambda.addToRolePolicy(snsPublishPolicy);
+*/
   }
 
-  createBucket(buckName: string):s3.IBucket{
+  createBucket(bucketName:string):s3.IBucket{
     const bucket = new s3.Bucket(this, 'ThumblingBucket', {
-      bucketName: buckName,
+      bucketName: bucketName,
       removalPolicy:cdk.RemovalPolicy.DESTROY
     });
     return bucket;
   }
+
 
   createLambda(functionPath:string, bucketName: string, folderInput: string, folderOutput: string):lambda.IFunction{
     const lambdaFunction = new lambda.Function(this, 'ThumbLambda',{
@@ -51,4 +75,66 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     });
     return lambdaFunction;
   }
+
+  createS3NotifyToLambda(prefix: string, lambda: lambda.IFunction, bucket: s3.IBucket): void {
+    const destination = new s3n.LambdaDestination(lambda);
+      bucket.addEventNotification(
+        s3.EventType.OBJECT_CREATED_PUT,
+        destination,
+        {prefix: prefix} //folder contains the original images
+    )
+  }
+/*
+  createS3NotifyToSns(prefix: string, snsTopic: sns.ITopic, bucket: s3.IBucket): void {
+    const destination = new s3n.SnsDestination(snsTopic)
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED_PUT, 
+      destination,
+      {prefix: prefix}
+    );
+  }
+
+  createSnsTopic(topicName: string): sns.ITopic{
+    const logicalName = "Topic";
+    const snsTopic = new sns.Topic(this, logicalName, {
+      topicName: topicName
+    });
+    return snsTopic;
+  }
+
+  createSnsSubscription(snsTopic: sns.ITopic, webhookUrl: string): sns.Subscription {
+    const snsSubscription = snsTopic.addSubscription(
+      new subscriptions.UrlSubscription(webhookUrl)
+    )
+    return snsSubscription;
+  }
+
+  createPolicyBucketAccess(bucketArn: string){
+    const s3ReadWritePolicy = new iam.PolicyStatement({
+      actions: [
+        's3:GetObject',
+        's3:PutObject',
+      ],
+      resources: [
+        `${bucketArn}/*`,
+      ]
+    });
+    return s3ReadWritePolicy;
+  }
+
+  createPolicySnSPublish(topicArn: string){
+    const snsPublishPolicy = new iam.PolicyStatement({
+      actions: [
+        'sns:Publish',
+      ],
+      resources: [
+        topicArn
+      ]
+    });
+    return snsPublishPolicy;
+  }
+
+  */
+
+
 }
