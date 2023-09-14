68ec30329b9a87341f0bb3bb04618a2446498916
Qin Xu <quinxu@hotmail.com>
2023-09-14 20:27:32 +0000

    updated query in backend files.

--- backend-flask/db/sql/users/show.sql
+++ backend-flask/db/sql/users/show.sql
@@ -1,8 +1,14 @@
 SELECT
-  users.uuid,
-  users.handle,
-  users.display_name,
-  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json)) array_row) as activities
-FROM public.users
+  b.uuid,
+  b.handle,
+  b.display_name,
+  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json)
+    SELECT a.uuid, b.display_name, b.handle, a.message, a.created_at, a.expires_at
+    FROM public.activities a 
+    where b.uuid = a.uuid
+    ORDER BY a.created_at DESC
+    limit 40
+ ) array_row) as activities
+FROM public.users b
 WHERE 
-  users.handle = %(handle)s
+  b.handle = %(handle)s
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -8,19 +8,22 @@ class UserActivities:
       'data': None
     }
 
-    now = datetime.now(timezone.utc).astimezone()
+    #now = datetime.now(timezone.utc).astimezone()
 
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
     else:
-      now = datetime.now()
-      results = [{
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
-        'created_at': (now - timedelta(days=1)).isoformat(),
-        'expires_at': (now + timedelta(days=31)).isoformat()
-      }]
+      sql= db.template('db/sql/activities', 'home.sql')
+      results = db.query_json_object_array(sql)
+      #now = datetime.now()
+      #results = [{
+      #  'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+      #  'handle':  'Andrew Brown',
+      #  'message': 'Cloud is fun!',
+      #  'created_at': (now - timedelta(days=1)).isoformat(),
+      #  'expires_at': (now + timedelta(days=31)).isoformat()
+      #}]
+      
       model['data'] = results
       # xray ---
       # subsegment = xray_recorder.begin_subsegment('mock-data')
